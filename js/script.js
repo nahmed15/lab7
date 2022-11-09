c007a6ae665408fd3e869b5b8882ed7a8290c27a
nahmed15 <112483909+nahmed15@users.noreply.github.com>
2022-11-09 00:33:15 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ function init() {
   //add your javascript between these two lines of code
   function Functionalert() {
     var input = document.getElementById('entryinput').value;
+    var output = document.getElementById('textoutput');
     alert("Nadia Ahmed: " + input.value);
     textoutput.innerHTML = input;
   }
